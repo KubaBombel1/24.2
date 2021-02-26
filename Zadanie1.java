a6bee02d0c0d0d8e9a7c6c8f94651811f6c416c5
KubaBombel1 <79452843+KubaBombel1@users.noreply.github.com>
2021-02-26 22:59:00 +0100

    Add files via upload

---
+++ Zadanie1.java
@@ -0,0 +1,30 @@
+
+package zadanie.pkg1;
+
+
+public class Zadanie1 {
+
+   
+       public static void main(String[] args) {
+
+        int i = -10;
+
+
+        while (i <= 40) 
+        
+        {
+
+            i++;
+
+
+
+     if (i % 2 == 1)
+
+            {
+
+
+    System.out.println(i);
+            }
+        }
+    }
+}
