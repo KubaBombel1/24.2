b453a3fae7bb940bb06d55268064c37fa13cba1b
KubaBombel1 <79452843+KubaBombel1@users.noreply.github.com>
2021-02-26 22:04:19 +0100

    Add files via upload

---
+++ Zadanie 1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zadanie.pkg1.pkg2;
+
+/**
+ *
+ * @author Kuba
+ */
+public class Zadanie12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i;
+        
+        i = 9;
+           
+     while (i++ < 20){   
+      System.out.println(i);   
+     }
+        
+    }
+    
+}
